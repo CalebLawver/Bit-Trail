3665bd3580e5d100290236a11adc771b201c67a8
Richard Caleb Lawver <clawver30@gmail.com>
2022-06-15 19:50:21 -0600

    pushing models

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 
 // routes go here
-
+const userRoutes = require('./user-routes');
+const trailRoutes = require('./trail-routes');
 
 // router.use('/route', ***routes)
+router.use('/users', userRoutes);
+router.use('trails', trailRoutes);
 
 module.exports = router;
---
+++ controllers/api/trail-routes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const sequelize = require('../../config/connection');
+const { Trail, User } = require('../../models');
+
+// get all trails
+router.get('/', (req, res) => {
+
+})
