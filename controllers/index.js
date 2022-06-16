eaf0f1a72a3e024cfdc7a769d19e24b598efe6a0
Richard Caleb Lawver <clawver30@gmail.com>
2022-06-15 19:30:01 -0600

    rough draft

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 // routes go here
 const apiRoutes = require('./api/');
+const homeRoutes = require('./home-routes');
 
 
 // router.use('/route', ***routes)
 router.use('./api', apiRoutes);
+router.use('/', homeRoutes);
 
 module.exports = router;
