6bf245813aabdb4e471637c83e5656af4c1cc78d
Merrick Watts <merricklwatts@gmail.com>
2022-06-15 20:32:06 -0600

    some inital seeds for trails

--- seeds/index.js
+++ seeds/index.js
@@ -1,1 +1,62 @@
 //import seeds for multiple tables
+
+const trails = [
+    {
+      name: 'Donut Falls Trail',
+      address: '12 Cottonwood Canyon Rd, Salt Lake City, UT 84121',
+      lat: '40.595690',
+      lon: '-111.581530',
+      miles: '3.3',
+      kilometers: '5.3',
+      difficulty: 'Easy',
+      blurb: 'A very easy and family/kids-friendly hiking trail.'
+    },
+    {
+        name: 'Living Room Lookout Trail',
+        address: 'Colorow Dr, Salt Lake City, UT 84108',
+        lat: '40.760110',
+        lon: '-111.822170',
+        miles: '2.4',
+        kilometers: '3.9',
+        difficulty: 'Medium',
+        blurb: 'Pay attention to the trails you go on, there are many offshoots that can be very challenging, especially to go down.'
+      },
+      {
+        name: 'Cecret Lake Trail',
+        address: 'Cecret Lake Trail, Alta, UT 84092',
+        lat: '40.574620',
+        lon: '-111.616360',
+        miles: '1.8',
+        kilometers: '2.9',
+        difficulty: 'Easy',
+        blurb: 'Short hike, beautiful scenery, chance to see moose and other animals.  Nice place to relax.  It`s fun to see the salamanders too. The kids like that.'
+      },
+      {
+        name: 'Lake Mary Trail',
+        address: 'Lake Mary Trail, Salt Lake City, UT 84121',
+        lat: '40.613530',
+        lon: '-111.808200',
+        miles: '2.6',
+        kilometers: '4.2',
+        difficulty: 'Medium',
+        blurb: 'DO IT!!!  EXPERIENCE OF A LIFETIME!!!'
+      },
+      {
+        name: 'Grandeur Peak East Trail from Church Fork',
+        address: 'Lake Mary Trail, Salt Lake City, UT 84121',
+        lat: '40.613530',
+        lon: '-111',
+        miles: '2.6',
+        kilometers: '4.2',
+        difficulty: 'Medium',
+        blurb: 'DO IT!!!  EXPERIENCE OF A LIFETIME!!!'
+      },
+  ];
+
+  const users = [
+    {
+        username: '',
+        email: '',
+        password: '',
+    }
+  ]
