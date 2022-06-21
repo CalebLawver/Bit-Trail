bfa823d804438dbfadbbeecb534f5dcd976ff89f
Richard Caleb Lawver <clawver30@gmail.com>
2022-06-20 19:04:43 -0600

    added index for seeding

--- seeds/index.js
+++ seeds/index.js
@@ -1,75 +1,17 @@
-//import seeds for multiple tables
+const seedUsers = require('./users-seeds');
+const seedTrails = require('./trail-seeds');
 
-const trails = [
-    {
-      name: 'Donut Falls Trail',
-      address: '12 Cottonwood Canyon Rd, Salt Lake City, UT 84121',
-      lat: '40.595690',
-      lon: '-111.581530',
-      miles: '3.3',
-      kilometers: '5.3',
-      difficulty: 'Easy',
-      blurb: 'A very easy and family/kids-friendly hiking trail.'
-    },
-    {
-        name: 'Living Room Lookout Trail',
-        address: 'Colorow Dr, Salt Lake City, UT 84108',
-        lat: '40.760110',
-        lon: '-111.822170',
-        miles: '2.4',
-        kilometers: '3.9',
-        difficulty: 'Medium',
-        blurb: 'Pay attention to the trails you go on, there are many offshoots that can be very challenging, especially to go down.'
-      },
-      {
-        name: 'Cecret Lake Trail',
-        address: 'Cecret Lake Trail, Alta, UT 84092',
-        lat: '40.574620',
-        lon: '-111.616360',
-        miles: '1.8',
-        kilometers: '2.9',
-        difficulty: 'Easy',
-        blurb: 'Short hike, beautiful scenery, chance to see moose and other animals.  Nice place to relax.  It`s fun to see the salamanders too. The kids like that.',
-        user_id: '1',
-      },
-      {
-        name: 'Lake Mary Trail',
-        address: 'Lake Mary Trail, Salt Lake City, UT 84121',
-        lat: '40.613530',
-        lon: '-111.808200',
-        miles: '2.6',
-        kilometers: '4.2',
-        difficulty: 'Medium',
-        blurb: 'DO IT!!!  EXPERIENCE OF A LIFETIME!!!',
-        user_id: '1',
-      },
-      {
-        name: 'Grandeur Peak East Trail from Church Fork',
-        address: 'Lake Mary Trail, Salt Lake City, UT 84121',
-        lat: '40.613530',
-        lon: '-111',
-        miles: '2.6',
-        kilometers: '4.2',
-        difficulty: 'Medium',
-        blurb: 'DO IT!!!  EXPERIENCE OF A LIFETIME!!!',
-        user_id: '1',
-      },
-  ];
+const sequelize = require('../config/connection');
 
-  const users = [
-    {
-        username: 'Mike',
-        email: 'mike@example.com',
-        password: 'mikesverysecretpassword1',
-    },
-    {
-        username: 'Emily',
-        email: 'emily@example.com',
-        password: 'notmydogsname2',
-    },
-    {
-        username: 'Frank',
-        email: 'frank@example.com',
-        password: 'password123',
-    },
-  ]
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log('----------');
+  await seedUsers();
+  console.log('----------');
+  await seedTrails();;
+  console.log('----------');
+
+  process.exit(0);
+};
+
+seedAll();
