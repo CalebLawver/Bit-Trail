28f8dbde95cc325f0b7bbea79ea25294ed5a41e7
merrickwatts <98593406+merrickwatts@users.noreply.github.com>
2022-06-20 18:56:32 -0600

    Add files via upload

---
+++ seeds/users-seeds.js
@@ -0,0 +1,17 @@
+const users = [
+    {
+        username: 'Mike',
+        email: 'mike@example.com',
+        password: 'mikesverysecretpassword1',
+    },
+    {
+        username: 'Emily',
+        email: 'emily@example.com',
+        password: 'notmydogsname2',
+    },
+    {
+        username: 'Frank',
+        email: 'frank@example.com',
+        password: 'password123',
+    },
+  ]
