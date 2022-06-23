9d5a9078ac01c8155921eb04ad4b795d5307a633
Merrick Watts <merricklwatts@gmail.com>
2022-06-22 19:45:35 -0600

    rendering for the body on homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
+const { index, Trail, User } = require('../models');
 
 router.get('/', (req, res) => {
     console.log('showing home page')
-    return res.render('homepage')
+    return res.render('homepage', )
 });
 
 module.exports = router;
