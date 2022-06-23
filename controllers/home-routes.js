06feb5a79ec01be736fbf89b03ea5c5ca70b73ae
Merrick Watts <merricklwatts@gmail.com>
2022-06-22 19:58:47 -0600

    moving commits to homepage branch for Hiro

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { index, Trail, User } = require('../models');
 
 router.get('/', (req, res) => {
     console.log('showing home page')
