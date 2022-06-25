82c915e4eceb5da177de3815ae4a395e369897b5
Richard Caleb Lawver <clawver30@gmail.com>
2022-06-24 20:17:45 -0600

    working on getting the logged in functionality working, then adding a dashboard for creating and editing posted trails

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,60 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Trail, Review, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, (req, res) => {
+    console.log(req.session);
+    Trail.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }, 
+            {
+                model: Review,
+                attributes: ['rev_text', 'rev_diff']
+            }
+        ]
+    })
+    .then(dbTrailData => {
+        const trails = dbTrailData.map(trail => trail.get({ plain: true }));
+        res.render('dashboard', { trails, loggedIn: true });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/edit/:id', withAuth, (req, res) => {
+    this.post.findByPk(req.params.id, {
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbTrailData => {
+        if (dbTrailData) {
+            const trail = dbTrailData.get({ plain: true });
+
+            res.render('edit-trail', {
+                trail,
+                loggedIn: true
+            });
+        } else {
+            res.status(404).end();
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -3,10 +3,12 @@ const router = require('express').Router();
 // routes go here
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes');
+const dashboardRoutes = require('./dashboard-routes');
 
 
 // router.use('/route', ***routes)
 router.use('/api', apiRoutes);
+router.use('/dashboard', dashboardRoutes);
 router.use('/', homeRoutes);
 
 module.exports = router;
