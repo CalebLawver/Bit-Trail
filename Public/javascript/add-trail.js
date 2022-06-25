eeb8dc0fd7423f7c1bf98e0a35aaa50e5539cff6
hkagei <k_dawg_1022@hotmail.com>
2022-06-24 18:14:40 -0600

    restructure creat-trail.handlebars

--- Public/javascript/add-trail.js
+++ Public/javascript/add-trail.js
@@ -1,6 +1,9 @@
 async function newFormHandler(event) {
     event.preventDefault();
 
+  const title = document.querySelector('input[name="post-title"]').value;
+  const post_url = document.querySelector('input[name="post-url"]').value;
+
     const response = await fetch(`/api/trails`, {
         method: 'POST',
         body: JSON.stringify({
