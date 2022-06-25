6ef84f24f7dabe2659ebd2ebf32dd7e78a185026
hkagei <k_dawg_1022@hotmail.com>
2022-06-24 18:02:23 -0600

    update add-trail.js functionality

---
+++ Public/javascript/add-trail.js
@@ -0,0 +1,30 @@
+async function newFormHandler(event) {
+    event.preventDefault();
+
+    const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({
+          name,
+          address,
+          miles,
+          kilometers,
+          lat,
+          lon,
+          difficulty,
+          blurb
+
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+    if (response.ok) {
+        document.location.replace('/homepage');
+      } else {
+        alert(response.statusText);
+      }
+    }
+    
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+    
