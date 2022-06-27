d11e4cd29bcaef622b60a7f31a4338f9e00efd0b
hkagei <k_dawg_1022@hotmail.com>
2022-06-26 22:05:38 -0600

    add password requirements in validation_schema.js

--- utils/validation_schema.js
+++ utils/validation_schema.js
@@ -3,7 +3,7 @@ const { schema } = require('../models/User')
 
 const authSchema = joi.object({
     email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).lowercase().required(),
-    password: Joi.string().min(6).required(),
+    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).min(6).required(),
 })
 
 schema.validate({});
